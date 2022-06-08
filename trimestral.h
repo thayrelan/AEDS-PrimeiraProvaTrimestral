17c767abfa7a76726bc7e5c77da33a608531426f
SophiaVeneres <sosoveneres@gmail.com>
2022-06-08 14:18:37 +0000

    Criação e transformação de funções, separação da main.c e da trimestral.c/trimestral.h, alteração, adesão e remoção de comentários. Modificação da lógica, agora passa a usar boleano.

---
+++ trimestral.h
@@ -0,0 +1,7 @@
+#define MAXIMO 1000
+
+void Reinicializa(_Bool *Estado, int *Total_inseridos, int *Total_Retirados);
+int Verifica_Fila_Prioridade(int *Entrada, int *Saida, int Total_Inseridos, int Total_Retirados);
+int Verifica_Fila(int *Entrada, int *Saida, int Total_Inseridos, int Total_Retirados);
+int Verifica_Pilha(int *Entrada, int *Saida, int Total_Inseridos, int Total_Retirados);
+void Imprime_Tipo(_Bool *Estado);
